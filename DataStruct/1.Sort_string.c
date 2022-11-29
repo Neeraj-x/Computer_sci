0c82cbf5334720f6fb94fb27cf41bcc45ecf46dd
Neeraj-x <74083197+Neeraj-x@users.noreply.github.com>
2022-11-29 11:10:14 +0530

    Add files via upload

---
+++ DataStruct/1.Sort_string.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	char bin[5][20];
+	int i,j;
+	printf("Enter the names to sort:\n\n");
+	for(i=0; i<5;i++)
+		scanf("%s",&bin[i]);
+	printf("sorted list of names:\n\n");
+	for(i=65; i<122; i++)
+	{
+		for(j=0; j<5; j++)
+		{
+			if(bin[j][0]==i)
+				printf("\n%s",bin[j]);
+		}
+	}
+}
